b9b732456107d334ed3c4725f920dd44ace927dd
jkl851 <jkl851@naver.com>
2021-10-07 17:16:38 +0900

    commit

--- helloweb/src/main/java/helloweb/HelloServlet.java
+++ helloweb/src/main/java/helloweb/HelloServlet.java
@@ -8,15 +8,22 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 public class HelloServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.setContentType("text/html; charset=utf-8");
+		
+		response.setContentType("text/html; charset=utf-8");
+		
 		PrintWriter pw = response.getWriter();
 		pw.print("<h1>Hello World</h1>");
-		
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		//doGet(request, response);
 	}
 
 }
